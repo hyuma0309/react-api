0eff92d3f187686847d900bdfbcd9e344b100ece
asadahyuma <asadahyuma@team-lab.com>
2020-06-22 19:49:54 +0900

    prettirの設定とREADMEの修正

--- src/components/ProductForm.js
+++ src/components/ProductForm.js
@@ -1,57 +1,53 @@
-import  React from 'react'
+import React from 'react';
 
 export default class ProductForm extends React.Component {
   constructor(props) {
-    super(props)
-    this.state ={
-      messages: []
-    }
+    super(props);
+    this.state = {
+      messages: [],
+    };
   }
 
   onSubmit = e => {
     e.preventDefault();
     const messages = [];
-    const form = e.target.form
-    const title = form.title.value
-    const desc = form.desc.value
-    const price = form.price.value
+    const form = e.target.form;
+    const title = form.title.value;
+    const desc = form.desc.value;
+    const price = form.price.value;
     const valid = document.getElementById('valid');
 
-        const titleLength = title.length;
-        const descLength = desc.length;
-        const priceLength = Number(price);
+    const titleLength = title.length;
+    const descLength = desc.length;
+    const priceLength = Number(price);
 
-        if (titleLength === 0 || 10 < titleLength) {
-            messages.push('タイトルは1〜10字で入力して下さい');
-        }
-        if (descLength === 0 || 500 < descLength) {
-            messages.push('説明は1〜500字で入力して下さい');
-        }
-        if (priceLength === 0) {
-            messages.push('価格は1円以上で入力して下さい');
-        }
+    if (titleLength === 0 || 10 < titleLength) {
+      messages.push('タイトルは1〜10字で入力して下さい');
+    }
+    if (descLength === 0 || 500 < descLength) {
+      messages.push('説明は1〜500字で入力して下さい');
+    }
+    if (priceLength === 0) {
+      messages.push('価格は1円以上で入力して下さい');
+    }
 
-    if(messages.length === 0 ){
-    this.props.add(title, desc, price)
-    this.setState({messages: []});
-  }else{
-    valid.disabled = true;
-    this.setState({messages: messages});
-  }
-
-
-  }
+    if (messages.length === 0) {
+      this.props.add(title, desc, price);
+      this.setState({ messages: [] });
+    } else {
+      valid.disabled = true;
+      this.setState({ messages: messages });
+    }
+  };
 
   render() {
-
-    const messageList = this.state.messages.map((message) => {
+    const messageList = this.state.messages.map(message => {
       return (
-       <ul key={message}>
-      <li>{message}</li>
-      </ul>
-      )
-})
-
+        <ul key={message}>
+          <li>{message}</li>
+        </ul>
+      );
+    });
 
     return (
       <div>
@@ -62,11 +58,13 @@ export default class ProductForm extends React.Component {
           <label>説明：</label>
           <input type="text" id="desc" name="desc" placeholder="1〜500字" />
           <label>価格</label>
-          <input type="number"  id="price" name="price" placeholder="最低1円" min="1"/>
-          <button type="submit" id="valid" onClick={this.onSubmit}>追加</button>
+          <input type="number" id="price" name="price" placeholder="最低1円" min="1" />
+          <button type="submit" id="valid" onClick={this.onSubmit}>
+            追加
+          </button>
         </form>
-       {messageList}
-        </div>
-    )
+        {messageList}
+      </div>
+    );
   }
 }
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,78 +1,76 @@
-import React from 'react'
+import React from 'react';
 import EditForm from './EditForm';
 
 export default class ProductList extends React.Component {
+  //商品の削除
+  delete = id => {
+    this.props.delete(id);
+  };
 
-//商品の削除
-  delete = (id) => {
-    this.props.delete(id)
-}
-
-//画像ファイルの読み取り
-handleFileChange = (id,e) => {
-    e.preventDefault()
-    let reader = new FileReader()
-    let file = e.target.files[0]
+  //画像ファイルの読み取り
+  handleFileChange = (id, e) => {
+    e.preventDefault();
+    let reader = new FileReader();
+    let file = e.target.files[0];
     reader.onloadend = () => {
-      this.props.file(id,  reader.result);
-    }
-    reader.readAsDataURL(file)
-  }
+      this.props.file(id, reader.result);
+    };
+    reader.readAsDataURL(file);
+  };
 
-//フォームの表示
-handleButton= (id,e) =>{
-    e.preventDefault()
-    this.props.editForm(id)
-  }
+  //フォームの表示
+  handleButton = (id, e) => {
+    e.preventDefault();
+    this.props.editForm(id);
+  };
 
-//商品の編集
-edit = (id,e) =>{
-    e.preventDefault()
-    const form = e.target.form
-    const title = form.title.value
-    const desc = form.desc.value
-    const price = form.price.value
+  //商品の編集
+  edit = (id, e) => {
+    e.preventDefault();
+    const form = e.target.form;
+    const title = form.title.value;
+    const desc = form.desc.value;
+    const price = form.price.value;
     const editProduct = {
-        title: title,
-        price: price,
-        desc: desc
-    }
-    this.props.edit(id,editProduct)
-}
-
+      title: title,
+      price: price,
+      desc: desc,
+    };
+    this.props.edit(id, editProduct);
+  };
 
   render() {
-const product =  this.props.products.map((product) => {
-        if (product.isVisible === true ){
-            return(
+    const product = this.props.products.map(product => {
+      if (product.isVisible === true) {
+        return (
           <ul key={product.id}>
-          <li>{product.title}</li>
-          <li>{product.desc}</li>
-          <li>{product.price}円</li>
-        <div>
-        <button type="submit" onClick={() => this.delete(product.id)}>削除</button>
-        <button type="submit" onClick = {(e) => this.handleButton(product.id , e)}>編集</button>
-        </div>
+            <li>{product.title}</li>
+            <li>{product.desc}</li>
+            <li>{product.price}円</li>
+            <div>
+              <button type="submit" onClick={() => this.delete(product.id)}>
+                削除
+              </button>
+              <button type="submit" onClick={e => this.handleButton(product.id, e)}>
+                編集
+              </button>
+            </div>
 
-        <div>
-        <input type="file" onChange={(e) => this.handleFileChange(product.id, e)}/>
-        <img src={product.imagePreviewUrl} />
-        </div>
+            <div>
+              <input type="file" onChange={e => this.handleFileChange(product.id, e)} />
+              <img src={product.imagePreviewUrl} />
+            </div>
 
-          {product.editIsVisible &&
-          <EditForm
-          product = {this.props.products}
-          edit = {this.edit}
-          id = {product.id}
-          />}
+            {product.editIsVisible && (
+              <EditForm product={this.props.products} edit={this.edit} id={product.id} />
+            )}
           </ul>
-            )
-        }
+        );
+      }
     });
-    const searchItem = this.props.products.filter( function( value ) {
-       return value.isVisible === true
-  })
-
+    const searchItem = this.props.products.filter(function(value) {
+      return value.isVisible === true;
+    });
 
     return (
       <div>
@@ -80,6 +78,6 @@ const product =  this.props.products.map((product) => {
         {product}
         <h3>商品件数は{searchItem.length}件です</h3>
       </div>
-    )
+    );
   }
 }
