e5a21cd013c6b1ddd47cb7952abf5ca0d7de0f5d
asadahyuma <asadahyuma@team-lab.com>
2020-09-07 22:22:41 +0900

    簡易サーバーの追加

---
+++ serve/build/precache-manifest.599f5f0db07ed95c16b39678b2014357.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "025f414235449daba6612f40c2b9b448",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d21ee5221ea29f509ac0",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5f7b94ba2a751963c62a",
+    "url": "/static/js/2.796bb445.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.796bb445.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d21ee5221ea29f509ac0",
+    "url": "/static/js/main.1cae426c.chunk.js"
+  },
+  {
+    "revision": "e7221be7c37c7dc6b9d2",
+    "url": "/static/js/runtime-main.50330f7e.js"
+  }
+]);
