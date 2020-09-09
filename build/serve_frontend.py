18e1d31d985ba7d4ad18c6ccef688f2f082bd4c8
asadahyuma <asadahyuma@team-lab.com>
2020-09-09 16:57:36 +0900

    元にもどす

--- build/serve_frontend.py
+++ build/serve_frontend.py
@@ -1,6 +1,6 @@
 import os, sys, urlparse, SimpleHTTPServer, BaseHTTPServer
 HOST='0.0.0.0'
-PORT=80
+PORT=3000
 class Handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_GET(self):
         request_file_path = urlparse.urlparse(self.path).path.strip('/')
