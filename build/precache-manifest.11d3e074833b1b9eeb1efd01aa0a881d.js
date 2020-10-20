7499540160c4d49ea7ffd9ed41993c087974d322
asadahyuma <asadahyuma@team-lab.com>
2020-10-20 18:40:40 +0900

    buildファイルを監視下に配置

---
+++ build/precache-manifest.11d3e074833b1b9eeb1efd01aa0a881d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99ee76e20a1c561dd3b01dd2d3e181dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b114ba4413731b4cdd6e",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5f7b94ba2a751963c62a",
+    "url": "/static/js/2.796bb445.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.796bb445.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b114ba4413731b4cdd6e",
+    "url": "/static/js/main.de5c8ef9.chunk.js"
+  },
+  {
+    "revision": "e7221be7c37c7dc6b9d2",
+    "url": "/static/js/runtime-main.50330f7e.js"
+  }
+]);
