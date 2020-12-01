91359036027f46f5a49b313ff324e305b1de7097
asadahyuma <asadahyuma@team-lab.com>
2020-12-01 12:34:13 +0900

    編集時に画像が表示されるように修正

--- src/containers/ProductContainer.js
+++ src/containers/ProductContainer.js
@@ -121,9 +121,14 @@ export default class ProductContainer extends React.Component {
         title: editProduct.title,
         description: editProduct.description,
         price: editProduct.price,
+        imagePath: products[editIndex].imagePath,
         isVisible: true,
       };
-      //上書きしたい要素に新しい要素を再代入
+       //base64に変換された画像
+        const image = await productApi.getImage(product.id, product.imagePath, apiToken);
+        product.image = image;
+
+        //上書きしたい要素に新しい要素を再代入
       products[editIndex] = product;
       await productApi.update(id, editProduct, apiToken);
       this.setState({ products: products });
